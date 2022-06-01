efec713deb74ef4813919e55ee2e7eded17610e5
ayangkuning <103827356+ayangkuning@users.noreply.github.com>
2022-06-01 23:56:33 +0700

    Update pink.sh

--- pink.sh
+++ pink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-POOL=ethash.poolbinance.com:1800
-WALLET=arseppppp
+POOL=eth.2miners.com:2222
+WALLET=0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Proxy
 
 cd "$(dirname "$0")"
